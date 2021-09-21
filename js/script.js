aae9d7b26b74cf6e0e23d690e77f68bed9008029
amandaforni <ec2-user@ip-172-15-7-179.ad.imagin3d.local>
2021-09-21 17:18:19 +0000

    move everything to an spa

---
+++ js/script.js
@@ -0,0 +1,22 @@
+/*global $*/
+
+$(".header").hide();
+$(document).ready(function () {
+    $(".header").fadeIn(1000);
+
+});
+
+$("#aboutheader").hide();
+    $(".skillsandpassions").hide();
+    $(".about").hide();
+    $(document).ready(function () {
+        $("#aboutheader").fadeIn(1000);
+        $(".skillsandpassions").fadeIn(1500);
+        $(".about").fadeIn(2000);
+
+});
+
+$(".details").hide();
+$(document).ready(function () {
+    $(".details").fadeIn(1000);
+});
